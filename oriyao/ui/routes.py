bc03662c1b993871137646ea5808e74b2f9dc2f0
ucloud <ylzhangyao@gmail.com>
2021-01-26 03:04:41 +0000

    MOD login require

--- oriyao/ui/routes.py
+++ oriyao/ui/routes.py
@@ -4,6 +4,6 @@ from flask_login import login_required
 
 
 @blueprint.route('/<template>')
-@login_required
+#@login_required
 def route_template(template):
     return render_template(template + '.html')
